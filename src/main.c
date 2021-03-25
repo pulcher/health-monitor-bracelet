1d45affb5c29ec096fc2d61b76ba5acaabb87d8f
Harold Pulcher <pulcher@pulcher.biz>
2021-03-24 21:07:06 -0500

    got the i2c talking to the heart rate monitor

--- src/main.c
+++ src/main.c
@@ -12,10 +12,13 @@
 #include <zephyr.h>
 #include "gui.h"
 #include "app_ble.h"
+#include <drivers/i2c.h>
 
 #define LOG_LEVEL CONFIG_LOG_DEFAULT_LEVEL
 #include <logging/log.h>
 LOG_MODULE_REGISTER(app);
+
+#define I2C_DEV "I2C_1"
 
 static void on_app_ble_event(app_ble_event_data_t * evt_data)
 {
@@ -50,6 +53,30 @@ void on_gui_event(gui_event_t *event)
 
 void main(void)
 {
+        const struct device *i2c_dev;
+	uint8_t cmp_data[16];
+	uint8_t data[16];
+	int i, ret;
+
+	i2c_dev = device_get_binding(I2C_DEV);
+	if (!i2c_dev) {
+		printk("I2C: Device driver not found.\n");
+		return;
+	}
+
+	uint8_t error = 0u;
+
+	i2c_configure(i2c_dev, I2C_SPEED_SET(I2C_SPEED_STANDARD));
+
+/*	printk("Value of NRF_TWIM3_NS->PSEL.SCL: %ld \n",NRF_TWIM3_NS->PSEL.SCL);
+	printk("Value of NRF_TWIM3_NS->PSEL.SDA: %ld \n",NRF_TWIM3_NS->PSEL.SDA);
+	printk("Value of NRF_TWIM3_NS->FREQUENCY: %ld \n",NRF_TWIM3_NS->FREQUENCY);
+	printk("26738688 -> 100k\n");
+*/
+	
+
+
+
 	gui_config_t gui_config = {.event_callback = on_gui_event};
 	gui_init(&gui_config);
 
@@ -63,6 +90,47 @@ void main(void)
 	gui_set_bt_state(GUI_BT_STATE_ADVERTISING);
 
 	while (1) {
-		k_sleep(K_MSEC(1000));
+          uint8_t error;
+          int nDevices;
+ 
+          printk("Scanning...\n");
+ 
+          nDevices = 0;
+          for(uint16_t address = 80; address < 90; address++ ) 
+          {
+            struct i2c_msg msgs[1];
+            uint8_t dst = 1;
+
+            msgs[0].buf = &dst;
+            msgs[0].len = 1U;
+            msgs[0].flags = I2C_MSG_WRITE | I2C_MSG_STOP;
+
+            error = i2c_transfer(i2c_dev, &msgs[0], 1, address);
+            printk("Target Address: %x(%d), Error Code: %d, ", address, address, error);
+            if (error == 0)
+            {
+              printk("I2C device found at address 0x");
+              if (address<16) 
+                printk("0");
+              printk("%x",address);
+ 
+              nDevices++;
+            }
+            /*else if (error==4) 
+            {
+              printk("Unknow error at address 0x");
+              if (address<16) 
+                printk("0");
+              printk(address);
+            }    */
+            printk("  !\n");
+          }
+          
+          if (nDevices == 0)
+            printk("No I2C devices found\n");
+          else
+            printk("done\n");
+ 
+          k_sleep(K_MSEC(3000));
 	}
 }
