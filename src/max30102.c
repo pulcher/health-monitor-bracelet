a7d60a2b052518ac95d1c722dc718882951d6255
Harold Pulcher <pulcher@pulcher.biz>
2021-04-09 23:20:41 -0500

    good compile checking execution

--- src/max30102.c
+++ src/max30102.c
@@ -31,6 +31,7 @@ void max30102_setPulseAmplitudeRed(struct device *i2c_dev, uint8_t amplitude);
 void max30102_setPulseAmplitudeIR(struct device *i2c_dev, uint8_t amplitude);
 void max30102_enableSlot(struct device *i2c_dev, uint8_t slotNumber, uint8_t device);
 void max30102_enableFIFORollover(struct device *i2c_dev);
+void max30102_resetFIFO(struct device *i2c_dev);
 
 // utility methods
 uint8_t max30102_readReg(struct device *i2c_dev, uint8_t reg, const void* pBuf, uint8_t size);
@@ -96,7 +97,7 @@ void max30102_sensorConfiguration(struct device *i2c_dev, uint8_t ledBrightness,
   }
 
   max30102_enableFIFORollover(i2c_dev); 
-  resetFIFO(); 
+  max30102_resetFIFO(i2c_dev); 
 }
 
 void max30102_setLEDMode(struct device *i2c_dev, uint8_t ledMode)
@@ -176,6 +177,14 @@ void max30102_enableFIFORollover(struct device *i2c_dev)
   max30102_readReg(i2c_dev, MAX30102_FIFOCONFIG, &FIFOReg, 1);
   FIFOReg.RollOver = 1;
   max30102_writeReg(i2c_dev, MAX30102_FIFOCONFIG, &FIFOReg, 1);
+}
+
+void max30102_resetFIFO(struct device *i2c_dev)
+{
+  uint8_t byteTemp = 0;
+  max30102_writeReg(i2c_dev, MAX30102_FIFOWRITEPTR, &byteTemp, 1);
+  max30102_writeReg(i2c_dev, MAX30102_FIFOOVERFLOW, &byteTemp, 1);
+  max30102_writeReg(i2c_dev, MAX30102_FIFOREADPTR, &byteTemp, 1);
 }
 
 // Utilities
--- src/max30102.h
+++ src/max30102.h
@@ -11,6 +11,12 @@ Using stuff from the the Arduino C++ Library for the MAX30102
 // Base address for the breakout board
 #define MAX30102_IIC_ADDRESS 0x57 //I2C Address
 
+//FIFO Registers
+#define MAX30102_FIFOWRITEPTR 0x04 //FIFO Write Pointer, The FIFO Write Pointer points to the location where the MAX30102 writes the next sample. This pointer advances for each sample pushed on to the FIFO. It can also be changed through the I2C interface when MODE[2:0] is 010, 011, or 111.
+#define MAX30102_FIFOOVERFLOW 0x05 //FIFO Overflow Counter, When the FIFO is full, samples are not pushed on to the FIFO, samples are lost. OVF_COUNTER counts the number of samples lost. It saturates at 0x1F. When a complete sample is “popped” (i.e., removal of old FIFO data and shifting the samples down) from the FIFO (when the read pointer advances), OVF_COUNTER is reset to zero.
+#define MAX30102_FIFOREADPTR 0x06 //FIFO Read Pointer, The FIFO Read Pointer points to the location from where the processor gets the next sample from the FIFO through the I2C interface. This advances each time a sample is popped from the FIFO. The processor can also write to this pointer after reading the samples to allow rereading samples from the FIFO if there is a data communication error.
+#define MAX30102_FIFODATA 0x07 //FIFO Data Register, The circular FIFO depth is 32 and can hold up to 32 samples of data. The sample size depends on the number of LED channels (a.k.a. channels) configured as active. As each channel signal is stored as a 3-byte data signal, the FIFO width can be 3 bytes or 6 bytes in size.
+
 //Configuraion Registers
 #define MAX30102_FIFOCONFIG 0x08 //FIFO Configuration
 #define MAX30102_MODECONFIG 0x09 //Mode Configuration
