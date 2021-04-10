bb481102b86c7d6bf715bdf3dd03f4649b20a4a3
Harold Pulcher <pulcher@pulcher.biz>
2021-04-09 21:56:59 -0500

    starting the sensorConfiguration method

--- src/max30102.c
+++ src/max30102.c
@@ -13,9 +13,20 @@ Coded specifically for the nRF5340DK
 struct i2c_msg msgs[1];
 uint8_t dst = 1;
 uint8_t error = -1;
+uint8_t _activeLEDs;
 
-// methods
-uint8_t max30102_read_reg(struct device *i2c_dev, uint8_t reg, void* pBuf, uint8_t size);
+// access methods
+bool is_max30102_available(struct device *i2c_dev);
+uint8_t get_max30102_part_id(struct device *i2c_dev);
+
+// confiuration methods
+void max30102_softReset(struct device *i2c_dev);
+void max30102_sensorConfiguration(struct device *i2c_dev, uint8_t ledBrightness, uint8_t sampleAverage, uint8_t ledMode, uint8_t sampleRate, uint8_t pulseWidth, uint8_t adcRange);
+
+// utility methods
+uint8_t max30102_readReg(struct device *i2c_dev, uint8_t reg, const void* pBuf, uint8_t size);
+int max30102_writeReg(struct device *i2c_dev, uint8_t reg, const void* pBuf, uint8_t size);
+uint8_t max30102_write_read_reg(struct device *i2c_dev, uint8_t reg, void* pBuf, uint8_t size);
 
 bool is_max30102_available(struct device *i2c_dev) {
 
@@ -35,12 +46,62 @@ uint8_t get_max30102_part_id(struct device *i2c_dev)
   return byteTemp;
 }
 
+void max30102_softReset(struct device *i2c_dev)
+{
+  sMode_t modeReg;
+  max30102_readReg(i2c_dev, MAX30102_MODECONFIG, &modeReg, 1);
+  modeReg.reset = 1;
+  max30102_writeReg(i2c_dev, MAX30102_MODECONFIG, &modeReg, 1);
+  uint32_t startTime = k_uptime_get();
+  while (k_uptime_get() - startTime < 100) {
+    max30102_readReg(i2c_dev, MAX30102_MODECONFIG, &modeReg, 1);
+    if (modeReg.reset == 0) break; 
+    k_sleep(K_MSEC(1));
+  }
+}
 
+void max30102_sensorConfiguration(struct device *i2c_dev, uint8_t ledBrightness, uint8_t sampleAverage,
+                                  uint8_t ledMode, uint8_t sampleRate, uint8_t pulseWidth,
+                                  uint8_t adcRange)
+{
+  setFIFOAverage(sampleAverage);
 
+  setADCRange(adcRange);
 
+  setSampleRate(sampleRate);
+
+  setPulseWidth(pulseWidth);
+
+  setPulseAmplitudeRed(ledBrightness);
+  setPulseAmplitudeIR(ledBrightness);
+
+  enableSlot(1, SLOT_RED_LED);
+  if (ledMode > MODE_REDONLY) enableSlot(2, SLOT_IR_LED);
+
+  setLEDMode(ledMode);
+
+  if (ledMode == MODE_REDONLY) {
+    _activeLEDs = 1;
+  } else {
+    _activeLEDs = 2;
+  }
+
+  enableFIFORollover(); 
+  resetFIFO(); 
+}
 
 
 // Utilities
+uint8_t max30102_readReg(struct device *i2c_dev, uint8_t reg, const void* pBuf, uint8_t size)
+{
+    return max30102_write_read_reg(i2c_dev, reg, pBuf, size);
+}
+
+int max30102_writeReg(struct device *i2c_dev, uint8_t reg, const void* pBuf, uint8_t size)
+{
+    return i2c_write(i2c_dev, pBuf, size, reg);
+}
+
 
 uint8_t max30102_write_read_reg(struct device *i2c_dev, uint8_t reg, void* pBuf, uint8_t size)
 {
--- src/max30102.h
+++ src/max30102.h
@@ -1,6 +1,8 @@
 /*
 Driver for MAX30102
 Coded specifically for the nRF5340DK
+
+Using stuff from the the Arduino C++ Library for the MAX30102
 */
 
 #ifndef _MAX30102_H
@@ -9,11 +11,68 @@ Coded specifically for the nRF5340DK
 // Base address for the breakout board
 #define MAX30102_IIC_ADDRESS 0x57 //I2C Address
 
+//Configuraion Registers
+#define MAX30102_MODECONFIG 0x09//Mode Configuration
+
 //Part ID Registers
 #define MAX30102_REVISIONID 0xFE //Revision ID
 #define MAX30102_PARTID 0xFF //Part ID:0x15
 #define MAX30102_EXPECTED_PARTID 0x15
 
+/*
+Mode configuration(0x09) (pg 18)
+* ------------------------------------------------------------------------------------------
+* |    b7    |    b6    |    b5    |    b4    |    b3    |    b2    |    b1     |    b0    |
+* ------------------------------------------------------------------------------------------
+* |   SHDN   |   RESET  |             NONE               |              MODE               |
+* ------------------------------------------------------------------------------------------
+*/
+typedef struct {
+uint8_t   ledMode:6; /*!< 010:Heart Rate mode, Red only. 011:SpO2 mode, Red and IR. 111:Multi-LED mode, Red and IR*/
+uint8_t   reset:1; /*!< 1:reset */
+uint8_t   shutDown:1; /*!< 0: wake up 1: put IC into low power mode*/
+} __attribute__ ((packed)) sMode_t;
 
+// Class Level defines
+//Configuration Options 
+//FIFO Configuration(Register address 0x08)
+//sampleAverage(Table 3. Sample Averaging)
+#define SAMPLEAVG_1     0
+#define SAMPLEAVG_2     1
+#define SAMPLEAVG_4     2
+#define SAMPLEAVG_8     3
+#define SAMPLEAVG_16    4
+#define SAMPLEAVG_32    5
 
+//Mode configuration(Register address 0x09)
+//ledMode(Table 4. Mode Control)
+#define MODE_REDONLY    2
+#define MODE_RED_IR     3
+#define MODE_MULTILED   7
+
+//Particle sensing configuration(Register address 0x0A)
+//adcRange(Table 5. SpO2 ADC Range Control)
+#define ADCRANGE_2048   0
+#define ADCRANGE_4096   1
+#define ADCRANGE_8192   2
+#define ADCRANGE_16384  3
+//sampleRate(Table 6. SpO2 Sample Rate Control)
+#define SAMPLERATE_50   0 
+#define SAMPLERATE_100  1
+#define SAMPLERATE_200  2
+#define SAMPLERATE_400  3
+#define SAMPLERATE_800  4
+#define SAMPLERATE_1000 5
+#define SAMPLERATE_1600 6
+#define SAMPLERATE_3200 7
+//pulseWidth(Table 7. LED Pulse Width Control)
+#define PULSEWIDTH_69   0 
+#define PULSEWIDTH_118  1
+#define PULSEWIDTH_215  2
+#define PULSEWIDTH_411  3
+
+//Multi-LED Mode Control Registers(Register address 0x011)
+#define SLOT_NONE       0
+#define SLOT_RED_LED    1
+#define SLOT_IR_LED     2
 #endif
